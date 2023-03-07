aa8526d53906712fd03d18eef1085b49865a7aa7
AvielSheto <avielsheto@gmail.com>
2023-03-07 16:25:36 +0200

    Creating navbar and made responsive mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="z-0">
+          <StarsCanvas />
+          <Contact />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
